9a8ba8ae2ead1086affb119bd85823c74c9c8f1b
PDOttavio <pablodottavio@gmail.com>
2025-09-13 18:48:01 -0300

    creo repositorio local

---
+++ practica.py
@@ -0,0 +1,13 @@
+print ("hola mundo")
+edad = 35
+experiencia = 4
+nombre = "Pablo"
+profesion = "desarrollador"
+enviado = True
+noenviado = False
+print("Mi edad es", edad)
+print("Mi tiempo de laburo es",experiencia)
+print("Mi nombre es", nombre)
+print("Soy", profesion)
+print("Enviado", enviado)   
+print("No enviado", noenviado)

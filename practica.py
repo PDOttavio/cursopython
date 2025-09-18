3fefe91bee3b248d55fd062e91fed94717fa9941
PDOttavio <pablodottavio@gmail.com>
2025-09-18 16:38:15 -0300

    fin de la clase de hoy

--- practica.py
+++ practica.py
@@ -1,13 +1,67 @@
-print ("hola mundo")
-edad = 35
-experiencia = 4
+#print ("hola mundo")
+#edad = 35
+#experiencia = 4
+#nombre = "Pablo"
+#profesion = "desarrollador"
+#enviado = True
+#noenviado = False
+#print("Mi edad es", edad)
+#print("Mi tiempo de laburo es",experiencia)
+#print("Mi nombre es", nombre)
+#print("Soy", profesion)
+#print("Enviado", enviado)   
+#print("No enviado", noenviado)
+
+# CONCATENAR Y MOSTRAR EL TEXTO
+
 nombre = "Pablo"
-profesion = "desarrollador"
-enviado = True
-noenviado = False
-print("Mi edad es", edad)
-print("Mi tiempo de laburo es",experiencia)
-print("Mi nombre es", nombre)
-print("Soy", profesion)
-print("Enviado", enviado)   
-print("No enviado", noenviado)
+edad = 35 
+
+#concatenar con comas
+#print("Hola soy", nombre, "y tengo", edad)
+
+#concatenar con signo +
+#print("Hola soy " + nombre + " Como estas? y tengo " + str(edad))
+
+#operaciones matematicas
+a=10 #int
+b=3 #int
+c=2.5 #float
+
+#print("suma", a+b)
+#print("resta", a-b)
+#print("multiplicacion", a*b)   
+#print("division", a/b) #decimales
+#print("division entera", a//b) #entera
+#print("resto", a%b)
+#print("potencia", a**b)
+
+#combinar operaciones
+
+total = (a-b)**b
+#print("Calculo total", total)
+
+#age= input("Mi edad es ")
+
+#print (type(age))
+
+#edades=int (input("Cual es tu edad? "))
+#print("El año que viene tendras", edades + 1)
+
+#altura=float (input("Cual es tu altura en metros? "))
+#print("Tu altura es", altura, "metros")
+#print("Tu altura en centimetros es", altura*100, "cm")
+
+#numero=30
+#print ("el numero es"  + str(numero))
+
+#numero1=int (input("Ingresa un numero: "))
+#numero2=int (input("Ingresa otro numero: "))
+#print ("La suma es: " + str(numero1 + numero2))
+
+#Area
+base=float (input("Ingresa la base del triangulo: "))
+altura=float (input("Ingresa la altura del triangulo: "))
+area=(base*altura)/2
+print ("El area del triangulo es: " + str(area))
+
